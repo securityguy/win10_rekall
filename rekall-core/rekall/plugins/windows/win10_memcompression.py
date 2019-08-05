41be4633a88dc6710c824626d0a609f7ff2c5803
MalwareMechanic <malwaremechanic@gmail.com>
2019-08-05 14:35:02 -0400

    Updated license information

--- rekall-core/rekall/plugins/windows/win10_memcompression.py
+++ rekall-core/rekall/plugins/windows/win10_memcompression.py
@@ -1,24 +1,22 @@
-# Rekall Memory Forensics
-# Copyright 2019 Google Inc. All Rights Reserved.
+# Copyright (C) 2019 FireEye, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 # Authors:
 # Sebastian Vogl <sebastian.vogl@FireEye.com>
 # Blaine Stancill <blaine.stancill@FireEye.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#
 
 """This file adds support for memory compression on Windows 10.
 
